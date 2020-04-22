18f62d6016b14f5f52efd0c1d2f8bbf84e052923
ABHINAV CHAWLA <abhinav.chawla@ibm.com>
2020-04-22 03:24:43 -0400

    add "/" status route to verify heroku deployment

--- config/config.js
+++ config/config.js
@@ -2,6 +2,7 @@ let dotenv = require("dotenv");
 
 // Set the NODE_ENV to 'development' by default
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
+process.env.HEROKU_ENV = process.env.HEROKU_ENV || "staging";
 
 const envFound = dotenv.config();
 if (!envFound) {
@@ -32,5 +33,7 @@ module.exports = {
    */
   api: {
     prefix: "/api"
-  }
+  },
+  node_env: process.env.NODE_ENV,
+  heroku_env: process.env.HEROKU_ENV,
 };
